f88cdcf4ed266dbaa9345942a7da809ba24ff5ee
oleg ostrovski <oleg.ostrovski@gmail.com>
2024-04-10 10:58:16 +0300

    fix App.tsx add get axios + error a mistake

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+import {AxiosError} from "axios";
+import {useAppDispatch} from "./app/hooks/redux";
+import {getBeerStore} from "./app/reducer/beerStore-reducer";
+import {HeaderBar} from "./components/HeaderBar";
+import {TablePage} from "./components/TablePage";
+import {FooterSite} from "./components/FooterSite";
+import {beerShopAPI} from "./api/api-beerShop";
+import {ErrorSnackbar} from "./components/ErrorSnackbar";
+import {setError} from "./app/reducer/error-reducer";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        beerShopAPI.getBeerAll()
+            .then(res => {
+                dispatch(getBeerStore(res.data));
+            })
+            .catch((err: AxiosError) => {
+                dispatch(setError(err.message))
+            })
+    }, [dispatch])
+
+    return (
+        <>
+            <HeaderBar/>
+            <TablePage/>
+            <FooterSite/>
+            <ErrorSnackbar/>
+        </>
+    );
 }
 
 export default App;
